27a1f22b647de73fc81c742bc498586110017baf
Vincent <vincent.dagoury@gmail.com>
2019-11-02 10:09:46 +0100

    Wip. Removing eslinting with webpack...

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: [
+          'babel-loader'
+          //  , 'eslint-loader'
+        ],
         type: 'javascript/auto'
       }
     ]
